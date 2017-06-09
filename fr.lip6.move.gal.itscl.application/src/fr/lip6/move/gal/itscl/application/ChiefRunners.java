fc37b72bcee67749ef90f3b63e8b2a1bb91829e5
Anissa Kheireddine <ani.kheireddine@hotmail.com>
2017-06-09 12:11:13 +0200

    replace Observable to chiefrunner

---
+++ fr.lip6.move.gal.itscl.application/src/fr/lip6/move/gal/itscl/application/ChiefRunners.java
@@ -0,0 +1,91 @@
+package fr.lip6.move.gal.itscl.application;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+
+public class ChiefRunners implements Callable<Boolean> , ISolverObservable {
+	
+	private Set<ISolver> obs = new HashSet<>();
+	private List <Future<Integer>> fsolvers=null;
+	private ExecutorService executor= Executors.newCachedThreadPool();
+	private int timeout;
+	private TimeUnit unit;
+	
+	
+	public void attach(ISolver o) {
+		obs.add(o);
+		
+	}
+
+	public void detach(ISolver o) {
+		obs.remove(o);
+	}
+	
+	public void setTimeout(int timeout,TimeUnit u){
+		this.timeout=timeout;
+		unit=u;
+	}
+	
+	
+
+	public boolean taskDone(int result){
+		if(result==0)
+			return true;
+		return false;
+	}
+	
+
+	public void killAll() {
+		List<Runnable> notFinished = executor.shutdownNow();  // savoir qui a fini ?
+		for (Future<Integer> o : fsolvers){
+			if(o.isDone()){
+				//celui qui a fini
+			}else if(!o.isCancelled()){
+				//Erreur un thread n'a pas ete shutdown
+				o.cancel(true); 
+			}
+			
+		}
+	}
+		
+		
+	
+	public Boolean call(){
+		try{
+			for(ISolver o : obs){
+				fsolvers.add(executor.submit(o));
+			}
+			int stillSolver=obs.size(), i=0;
+			CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(executor);
+		
+			do{
+				Future<Integer> solverDone=completionService.poll(timeout,unit);	
+				if (solverDone.isDone() && taskDone(solverDone.get())){
+					killAll();
+					return true;
+				}
+				i++;
+				
+			}while(i<stillSolver);
+			
+		}catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return false;
+	}
+	
+
+
+}
